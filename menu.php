3439d63698b1846b360ddb0fd51031139a48572d
AyomaPerera <86101811+AyomaPerera@users.noreply.github.com>
2022-08-15 12:06:41 +0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,37 @@
+<?php 
+
+    include('../config/constant.php'); 
+    include('login-check.php');
+
+
+?>
+
+
+
+
+
+<html>
+    <head>
+        <title>HeaveanlyBee-AdminHome</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+
+    </head>
+
+    <body>
+        <!--Menue Section Start-->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="Manage-admin.php">Admin</a></li>
+                    <li><a href="Manage-Services.php">Services</a></li>
+                    <li><a href="ManageMC.php">Ayurvedic Places</a></li>
+                    <li><a href="ManageProducts.php">Products</a></li>
+                    <li><a href="Logout.php">Logout</a></li>
+                </ul>
+            </div>
+            
+
+        </div>
+        <!--Menue Section End-->
